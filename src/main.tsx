aa64fbe5fe8ee70fc1d7f9f8dcca5c1780b460cc
KLertprapamongkol <KLertprapamongkol@THBKK04ITSN004>
2026-07-30 13:48:49 +0700

    feat: add orange field editing theme

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './layout-fixes.css';
+import './orange-theme.css';
 
 createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>);
