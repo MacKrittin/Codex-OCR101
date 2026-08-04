56c84badc763834684a377261e1cf424c2588594
KLertprapamongkol <KLertprapamongkol@THBKK04ITSN004>
2026-08-04 13:09:39 +0700

    feat: add default shipping fields

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App, { createDefaultFields } from './App';
 
 test('shows the empty document workspace', () => {
   render(<App />);
   expect(screen.getByRole('heading', { name: /pdf field extractor/i })).toBeInTheDocument();
   expect(screen.getByLabelText(/upload pdf/i)).toBeInTheDocument();
+});
+
+test('creates the standard shipping fields in order', () => {
+  expect(createDefaultFields().map((field) => field.name)).toEqual([
+    'Shipper', 'Consignee', 'Notify Party', 'Description of Goods', 'Shipping Marks', 'Total', 'Weight',
+  ]);
 });
